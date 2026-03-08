73808213c76e0f0602ea5c8df427d19e45651f27
egrissino <evanjgrissino@gmail.com>
2026-03-08 15:35:56 -0400

    build opts

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,8 @@ RUN apt-get install -y build-essential
 RUN apt-get install -y chrpath diffstat gawk lz4 locales
 RUN apt-get install -y cpio file git wget zstd nano screen iputils-ping iproute2
 RUN apt-get install -y python3 python3-pip python3-venv
-RUN apt-get install -y repo
+RUN apt-get install -y repo libncurses-dev pkg-config
+
 RUN apt-get install git
 RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
 
@@ -19,9 +20,8 @@ ENV LANG=en_US.UTF-8
 ENV LANGUAGE=en_US:en  
 ENV LC_ALL=en_US.UTF-8
 
+RUN userdel ubuntu
 RUN adduser ${USER}
-RUN usermod -aG ubuntu ${USER}
-RUN newgrp ubuntu
 
 RUN git config --global user.email "${EMAIL}"
 RUN git config --global user.name "${UNAME}"
--- buildyocto.sh
+++ buildyocto.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 RELEASE=walnascar
 
+if [[ -z $KERNEL ]]; then
+    KERNEL=linux-mono
+fi
+
+if [[ -z $IMAGE ]]; then
+    IMAGE=monogateway-image
+fi
+
 if [[ ! -z "$DRY_RUN" ]]; then
     BITBAKE_OPTS+=" --dry-run"
 fi
@@ -15,12 +23,14 @@ fi
 
 if [[ ! -z "$INIT" ]]; then
     # Download sources
-    repo init -u https://github.com/nxp-qoriq/yocto-sdk -b walnascar
+    if [[ "$INIT" -eq "repo" ]]; then
+        repo init -u https://github.com/nxp-qoriq/yocto-sdk -b walnascar
+    fi
     repo sync
 
     cd sources
-    git clone https://github.com/egrissino/meta-monogateway.git
-
+    git clone https://github.com/egrissino/meta-monogateway.git -b MonoSupport
+    git clone https://github.com/we-are-mono/meta-mono.git
     cd ../
 fi
 
@@ -29,8 +39,12 @@ mkdir -p build/conf
 
 cp ./sources/meta-monogateway/conf/local.conf $BUILD_DIR/conf/local.conf
 cp ./sources/meta-monogateway/conf/bblayers.conf $BUILD_DIR/conf/bblayers.conf
-cp -r ./sources/meta-monogateway/conf/machine/* $BUILD_DIR/conf/machine
 
 source sources/poky/oe-init-build-env
 
-bitbake core-image-minimal $BITBAKE_OPTS
+if [[ ! -z "$CLEAN" ]]; then
+    bitbake -c cleanall $KERNEL
+    bitbake -c cleanall $IMAGE
+fi
+
+bitbake $IMAGE $BITBAKE_OPTS
